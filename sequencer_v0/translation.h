fbd17275c727156252cad8e20efe27da6ddbfdcb
T Czerwonka <tczerwonka@gmail.com>
2020-06-28 21:30:05 -0500

    struct to handle i2c requests and map to text

---
+++ sequencer_v0/translation.h
@@ -0,0 +1,19 @@
+/******************************************************************************/
+/* translation - ic2 to actions */
+/******************************************************************************/
+
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+struct xlate_tbl {
+    char band[9];
+    uint8_t i2c;
+};
+
+static const uint8_t xlate_tbl_entries = 3;
+static const xlate_tbl i2ccmd[] = {
+  {"2m", 0x01},
+  {"222", 0x02}
+};
+
+#endif
