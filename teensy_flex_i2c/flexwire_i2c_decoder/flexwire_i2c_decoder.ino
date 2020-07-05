2e7cb41dcc7e0920812cf916df788c4e30fc23c5
T Czerwonka <tczerwonka@gmail.com>
2020-07-04 23:10:35 -0500

    initial commit

---
+++ teensy_flex_i2c/flexwire_i2c_decoder/flexwire_i2c_decoder.ino
@@ -0,0 +1,19 @@
+/******************************************************************************/
+/* flexwire_i2c_decoder                                                       */
+/*    Utility to decode output from FlexRadio systems Flexwire I2C interface  */
+/*    This works only on the Teensy-series of boards, tested with a 3.2.      */
+/*    This uses the https://github.com/nox771/i2c_t3 enhanced i2c library.    */
+/*    You will need to put i2c_t3.cpp and i2c_t3.h in this directory to build.*/
+/******************************************************************************/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
