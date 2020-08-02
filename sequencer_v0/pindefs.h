33c96b551775cb2d6fd3579890a0a852acaafc87
T Czerwonka <tczerwonka@gmail.com>
2020-08-01 22:57:39 -0500

    pinouts for relay boards

--- sequencer_v0/pindefs.h
+++ sequencer_v0/pindefs.h
@@ -3,7 +3,8 @@
 #ifndef _PINDEFS_H_
 #define _PINDEFS_H_
 
-
+//SDR1000 control pins
+//for legacy reference
 #define CONTROL1 53
 #define CONTROL2 51
 #define CONTROL3 49
@@ -12,6 +13,30 @@
 #define CONTROL6 43
 #define PTT_ACTIVE 41
 
+//RF relay control pins
+//1-4 are 28MHz
+//5-8 are 144MHz
+#define RF1A	23
+#define RF2A	25
+#define RF3A	27
+#define RF4A	29
+#define RF5A	31
+#define RF6A	33
+#define RF7A	35
+#define RF8A	37
+
+//12V relay control pins
+#define DC12RY1	39
+#define DC12RY2	41
+#define DC12RY3	43
+#define DC12RY4	45
+#define DC12RY5	47
+#define DC12RY6	49
+#define DC12RY7	51
+#define DC12RY8	53
+
+
+//PCA9555 pins
 #define IO0_0	36
 #define IO0_1	34
 #define IO0_2	32
