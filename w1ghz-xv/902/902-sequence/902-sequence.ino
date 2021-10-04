629ece9e6bf9ad43a60db769747b18d0c98a65bd
T Czerwonka <tczerwonka@gmail.com>
2021-10-03 19:05:45 -0500

    first pass

--- w1ghz-xv/902/902-sequence/902-sequence.ino
+++ w1ghz-xv/902/902-sequence/902-sequence.ino
@@ -1,9 +1,50 @@
-void setup() {
-  // put your setup code here, to run once:
+////////////////////////////////////////////////////////////////////////////////
+// 902 sequencer
+// https://github.com/tczerwonka/sequencer-sdr1k/
+// this is an arduino pro board
+////////////////////////////////////////////////////////////////////////////////
 
+#define PWR_LED   2
+#define PTT_LED   3
+#define PTT_IN    4
+#define TR_RELAY  5
+#define MMIC_PWR  6
+#define TX  1
+#define RX  0
+
+int current = RX;
+int previous = RX;
+
+
+void setup() {
+  pinMode(PWR_LED, OUTPUT);
+  pinMode(PTT_LED, OUTPUT);
+  pinMode(TR_RELAY, OUTPUT);
+  pinMode(MMIC_PWR, OUTPUT);
+  pinMode(PTT_IN, INPUT);
+
+  digitalWrite(PWR_LED, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (digitalRead(PTT_IN)) {
+    if (previous == RX) {
+      //go to TX
+      current = RX;
+      digitalWrite(PTT_LED, HIGH);
+      digitalWrite(TR_RELAY, HIGH);
+      delay(10);
+      digitalWrite(MMIC_PWR, HIGH);
+      previous = TX;
+    } //prev == RX
+  } else {
+    if (previous == TX) {
+      //go to RX
+      digitalWrite(MMIC_PWR, LOW);
+      delay(10);
+      digitalWrite(TR_RELAY, LOW);
+      digitalWrite(PTT_LED, LOW);
+      previous = RX;
+    }
+  }
 }
