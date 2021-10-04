0e75c00b1bbdc96bc4c5e1da1ebc3a5c11d7a6d5
T Czerwonka <tczerwonka@gmail.com>
2021-10-03 20:36:57 -0500

    working

--- w1ghz-xv/902/902-sequence/902-sequence.ino
+++ w1ghz-xv/902/902-sequence/902-sequence.ino
@@ -18,20 +18,22 @@ int previous = RX;
 
 void setup() {
   pinMode(PWR_LED, OUTPUT);
+  pinMode(13, OUTPUT);
   pinMode(PTT_LED, OUTPUT);
   pinMode(TR_RELAY, OUTPUT);
   pinMode(MMIC_PWR, OUTPUT);
-  pinMode(PTT_IN, INPUT);
+  pinMode(PTT_IN, INPUT_PULLUP);
 
   digitalWrite(PWR_LED, HIGH);
 }
 
 void loop() {
-  if (digitalRead(PTT_IN)) {
+  if (!digitalRead(PTT_IN)) {
     if (previous == RX) {
       //go to TX
       current = RX;
       digitalWrite(PTT_LED, HIGH);
+      digitalWrite(13, HIGH);
       digitalWrite(TR_RELAY, HIGH);
       delay(10);
       digitalWrite(MMIC_PWR, HIGH);
@@ -44,7 +46,9 @@ void loop() {
       delay(10);
       digitalWrite(TR_RELAY, LOW);
       digitalWrite(PTT_LED, LOW);
+      digitalWrite(13, LOW);
       previous = RX;
+      delay(500);
     }
   }
 }
